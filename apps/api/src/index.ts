fe98b63cc9ffdc2a6465bc0486ae8260b17fbe6d
Breno <brenluz@outlook.com>
2026-03-24 21:51:57 -0300

    Otimized permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,9 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'member' })
 
 const userCanInvite = ability.can('invite', 'User')
+const userCanDelete = ability.can('delete', 'User')
 
 console.log(userCanInvite)
+console.log(userCanDelete)
